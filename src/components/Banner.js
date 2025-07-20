b039e9042b7c955a043707224e1720e6365a43b4
Jbae1004 <sophiaoriginal96@gmail.com>
2025-07-19 21:14:09 -0800

    NavBar marginTop fixed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,95 +1,96 @@
 import React from "react";
-import logo from "../assets/image/logo.png";
+import logo from "../assets/image/LogoR.png";
 import { TypeAnimation } from "react-type-animation";
+
 
 export const Banner = () => {
   return (
     <section>
+      {/* EDIT to down -> Left */}
       <div
-        style={{
+        style={{ //
           display: "grid",
+          marginTop: '70px',
           gridTemplateColumns: "1fr", // default for mobile
         }}
       >
-
-        {/* Left content */}
-        <div
-          style={{
-            gridColumn: "span 1",
-            alignSelf: "center",
-            padding: "1rem",
-          }}
-        >
+        <div>
           <h1
-  style={{
-    marginBottom: "1rem",
-    fontSize: "2.25rem",
-    fontWeight: 800,
-    color: "white", // Hi! I'm will be white
-  }}
->
-  Hello! I'm{" "}
-  <span
-    style={{
-      backgroundImage: "linear-gradient(to right, #9CAEA9, #CCDAD1)",
-      WebkitBackgroundClip: "text",
-      WebkitTextFillColor: "transparent",
-      backgroundClip: "text",
-      display: "inline-block",
-      color: "transparent",
-      fontWeight: "700",
-    }}
-  >
-    <TypeAnimation
-      sequence={[
-        "Jiwon",
-        500,
-        "a web developer",
-        1000,
-        "a Software Engineer",
-        1000,
-        () => {},
-      ]}
-      wrapper="span"
-      cursor={true}
-      repeat={Infinity}
-      style={{ color: "inherit" }}
-      speed={50}
-    />
-  </span>
-</h1>
+            style={{
+              marginBottom: "1rem",
+              fontSize: "2.25rem",
+              fontWeight: 800,
+              color: "white",
+            }}
+          >
+            Hello! I'm{" "}
+            <span
+              style={{
+                backgroundImage: "linear-gradient(to right, #9CAEA9, #CCDAD1)",
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+                backgroundClip: "text",
+                display: "inline-block",
+                fontWeight: "700",
+              }}
+            >
+              <TypeAnimation
+                sequence={[
+                  "Jiwon",
+                  500,
+                  "a web developer",
+                  1000,
+                  "a Software Engineer",
+                  1000,
+                  "지원",
+                  1000,
+                  () => {},
+                ]}
+                wrapper="span"
+                cursor={true}
+                repeat={Infinity}
+                style={{ color: "inherit" }}
+                speed={50}
+              />
+            </span>
+          </h1>
 
           <p
             style={{
-              color: "#ADB7BE",
+              color: "white",
+              fontSize: "1.1rem"
             }}
           >
             BLAH BLAH
           </p>
         </div>
 
-        {/* Right graphic + image */}
-        <div
-          style={{
-            borderRadius: "9999px",
-            backgroundColor: "#181818",
-            width: "500px",
-            height: "400px",
-            position: "relative",
-          }}
-        >
-          <img
-            src={logo}
-            alt="Portfolio Logo"
-            style={{
-              position: "absolute",
-              transform: "translate(0, 0)",
-              width: "300px",
-              height: "300px",
-            }}
-          />
-        </div>
-      </div>
+
+  <div
+    style={{
+      width: "400px",
+      height: "400px",
+      borderRadius: "50%",
+      backgroundColor: "#222",
+      margin: "auto",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    }}
+  >
+    <img
+      src={logo}
+      alt="Portfolio Logo"
+      style={{
+        width: "300px",
+        height: "300px",
+        objectFit: "contain"
+      }}
+    />
+  </div>
+</div>
+
+
     </section>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,12 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/image/logo.png';
-import navIcon1 from '../assets/image/nav-icon1.svg';
-import navIcon2 from '../assets/image/nav-icon2.svg';
-
+import logo from "../assets/image/logo.png";
+import navIcon1 from "../assets/image/nav-icon1.svg";
+import navIcon2 from "../assets/image/nav-icon2.svg";
 
 export const NavBar = () => {
-  const [ activeLink, setActiveLink ] = useState("home");
-  const [ scrolled, setScrolled ] = useState(false);
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
     const onScroll = () => {
@@ -18,29 +17,29 @@ export const NavBar = () => {
     return () => window.removeEventListener("scroll", onScroll);
   }, []);
 
-    const onUpdateActiveLink = (value) => setActiveLink(value);
-  
-const iconContainerStyle = {
-  width: "42px",           // fixed circle size
-  height: "42px",
-  borderRadius: "50%",
-  border: "1px solid #fff",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  marginRight: "6px",
-  backgroundColor: "rgba(217, 217, 217, 0.1)", // optional bg
-};
-const iconImageStyle = {
-  width: "16px",          // smaller icon size inside
-  height: "16px",
-};
+  const onUpdateActiveLink = (value) => setActiveLink(value);
+
+  const iconContainerStyle = {
+    width: "42px", // fixed circle size
+    height: "42px",
+    borderRadius: "50%",
+    border: "1px solid #fff",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: "6px",
+    backgroundColor: "rgba(217, 217, 217, 0.1)",
+  };
+  const iconImageStyle = {
+    width: "16px",
+    height: "16px",
+  };
 
   return (
-    <Navbar expand="md" className={scrolled ? "scrolled": ""}>
+    <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="/">
-            <img src={logo} alt="Portfolio Logo" />
+          <img src={logo} alt="Portfolio Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggle-icon"></span>
@@ -49,30 +48,41 @@ const iconImageStyle = {
           <Nav className="me-auto">
             <Nav.Link
               href="#home"
-              className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}
-              onClick={() => onUpdateActiveLink('home')}
-            >Home</Nav.Link>
+              className={
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("home")}
+            >
+              Home
+            </Nav.Link>
             <Nav.Link
               href="#about"
-              className={activeLink === 'about' ? 'active navbar-link' : 'navbar-link'}
-              onClick={() => onUpdateActiveLink('about')}
-            >About</Nav.Link>
+              className={
+                activeLink === "about" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("about")}
+            >
+              About
+            </Nav.Link>
             <Nav.Link
               href="#skills"
-              className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'}
-              onClick={() => onUpdateActiveLink('skills')}
-            >Skills</Nav.Link>
-            </Nav>
+              className={
+                activeLink === "skills" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("skills")}
+            >
+              Skills
+            </Nav.Link>
+          </Nav>
           <span className="navbar-text">
-
             <div style={{ display: "flex" }}>
-    <a href="#" style={iconContainerStyle}>
-      <img src={navIcon1} alt="icon1" style={iconImageStyle} />
-    </a>
-    <a href="#" style={iconContainerStyle}>
-      <img src={navIcon2} alt="icon2" style={iconImageStyle} />
-    </a>
-  </div>
+              <a href="#" style={iconContainerStyle}>
+                <img src={navIcon1} alt="icon1" style={iconImageStyle} />
+              </a>
+              <a href="#" style={iconContainerStyle}>
+                <img src={navIcon2} alt="icon2" style={iconImageStyle} />
+              </a>
+            </div>
             <button className="vvd" onClick={() => console.log("contact")}>
               <span>Contact</span>
             </button>
@@ -81,4 +91,4 @@ const iconImageStyle = {
       </Container>
     </Navbar>
   );
-}
+};
