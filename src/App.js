1a4e81fe9bd4895f22eba1270cf21998c5038cb8
Jbae1004 <sophiaoriginal96@gmail.com>
2025-08-07 16:06:57 -0800

    index.js background color set

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { Banner } from './components/Banner/Banner';
 import { Footer } from './components/Footer/Footer';
 import Contact from './pages/Contact/Contact';
 import About from './pages/About/About';
+import styles from './App.module.scss'
 
 function App() {
   return (
-    <div>
+    <div className={styles.container}>
       <NavBar />
       <Banner />
       <About />
