201cb3a3f403785970be826892516402b8c87093
Jbae1004 <sophiaoriginal96@gmail.com>
2025-08-08 11:04:40 -0800

    Border around App.module.css update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import { NavBar } from './components/Navbar/NavBar';
-import { Banner } from './components/Banner/Banner';
-import { Footer } from './components/Footer/Footer';
-import Contact from './pages/Contact/Contact';
-import About from './pages/About/About';
-import styles from './App.module.scss'
+import { NavBar } from "./components/Navbar/NavBar";
+import { Banner } from "./components/Banner/Banner";
+import { Footer } from "./components/Footer/Footer";
+import Contact from "./pages/Contact/Contact";
+import About from "./pages/About/About";
+import styles from "./App.module.scss";
 
-function App() {
+export default function App() {
   return (
-    <div className={styles.container}>
-      <NavBar />
-      <Banner />
-      <About />
-      <Contact />
-      <Footer />
+    <div className={styles.body}>
+      <div className={styles.leftBorder} />
+      <div className={styles.rightBorder} />
+      <div className={styles.topBorder} />
+      <div className={styles.bottomBorder} />
+      <div className={styles.container}>
+        <NavBar />
+        <Banner />
+        <About />
+        <Contact />
+        <Footer />
+      </div>
     </div>
   );
 }
-
-export default App;
--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,3 +1,5 @@
 export { default as logo } from './logo.png'
 export { default as logo2 } from './logo2.png'
 export { default as logo3 } from './logo3.png'
+export { default as lr } from './LRborder.png'
+export { default as tb } from './TBborder.png'
