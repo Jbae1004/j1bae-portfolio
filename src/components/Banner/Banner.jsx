15c768f36fda89e26769b54a88439cdc1df740a1
Jbae1004 <sophiaoriginal96@gmail.com>
2025-08-14 01:08:58 -0800

    Issues with screen size fixed. Parallex motion works

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-// import { logo2 } from "../../assets/images";
+import { logo2 } from "../../assets/images";
 import styles from "./Banner.module.scss";
 
 export const Banner = () => {
   return (
     <section id="home" className={styles.banner}>
+      <img src={logo2} alt="Portfolio Logo" className={styles.bannerImage} />
       <div className={styles.textContainer}>
         <h1 className={styles.gaegu}>
           Hi, I&apos;m&nbsp;Jiwon</h1>
