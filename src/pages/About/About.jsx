7f622b774903641a7ebb3a5bfa3398e7c4e27697
Jbae1004 <sophiaoriginal96@gmail.com>
2025-08-20 17:09:48 -0800

    Need to redo About animation and profile.

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,30 +4,27 @@ import styles from "./About.module.scss";
 export const About = () => {
     return (
         <div id="about" className={styles.about}>
-            <div className={styles.stack}>
-                <img src={Profile} alt="Profile" className={styles.profile} />
-                <img src={Frame} alt="Frame" className={styles.frame} />
-            </div>
             <div className={styles.textContainer}>
-                <h2 className={styles.header}>Jiwon here!</h2>
-                <p className={styles.Fredoka}>
-                    I&apos;m Jiwon Bae, a recent computer science graduate and front-end
-                    junior who loves to build, design, and learn. I&apos;m always looking
-                    to grow and improve as a developer.
-                </p>
-                <p className={styles.Fredoka}>
-                    I&apos;m a quick learner, a team player, and passionate about creating
-                    interactive, user-friendly experiences. Beyond coding, I&apos;m
-                    curious about technology, love art, and enjoy bringing designs to life
-                    in the browser.
-                </p>
-                <p className={styles.Fredoka}>
-                    I&apos;m excited to bring my enthusiasm to a front-end development
-                    role where I can continue learning, growing, and creating meaningful
-                    experiences.
-                </p>
+                <img src={Frame} alt="" aria-hidden="true" className={styles.frame} />
+                <div className={styles.textContent}>
+                    <p className={styles.Fredoka}>
+                        I&apos;m Jiwon Bae, a recent computer science graduate and
+                        front-end junior who loves to build, design, and learn. I&apos;m
+                        always looking to grow and improve as a developer. I&apos;m a
+                        quick learner, a team player, and passionate about creating
+                        interactive, user-friendly experiences.
+                    </p>
+                    <p className={styles.Fredoka}>
+                        Beyond coding, I&apos;m curious about technology, love art, and
+                        enjoy bringing designs to life in the browser. I&apos;m excited to
+                        bring my enthusiasm to a front-end development role where I can
+                        continue learning, growing, and creating meaningful experiences.
+                    </p>
+                </div>
             </div>
+            <img src={Profile} alt="Profile" className={styles.profile} />
         </div>
+
     );
 };
 
